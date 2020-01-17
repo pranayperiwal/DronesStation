d538cea6faa8562907f7a87e621f40160cd9603d
pranayperiwal <56786002+pranayperiwal@users.noreply.github.com>
2020-01-17 18:22:36 +0530

    Add files via upload

--- drones.py
+++ drones.py
@@ -11,52 +11,6 @@ ex, ey = map(int, input().split())
 stations.append((ex, ey))
 path = False
 print(stations)
-#
-# path = False
-#
-# s = e = counter = 0
-# while s < len(stations):
-#     if s == len(stations):
-#         break
-#     if e == len(stations) and path:
-#         break
-#     counter += 1
-#     s = e = counter
-#     distance = math.sqrt(((stations[0][0] - stations[s][0]) ** 2) + (stations[0][1] - stations[s][1]) ** 2)
-#     print("0", s, e, distance, path, counter)
-#     if distance <= D:
-#         path = True
-#         e += 1
-#     else:
-#         path = False
-#         s += 1
-#         e += 1
-#         continue
-#     i = 0
-#     while e <= len(stations):
-#         if e == len(stations) and path:
-#             break
-#         if e == len(stations) and s < len(stations):
-#             e = s + 1
-#             s = counter
-#             continue
-#         print(path)
-#         distance = math.sqrt(((stations[s][0] - stations[e][0]) ** 2) + (stations[s][1] - stations[e][1]) ** 2)
-#         print(s, e, distance, counter, i)
-#         if distance <= D:
-#             s = e
-#             e += 1
-#             path = True
-#             continue
-#         else:
-#             path = False
-#             e += 1
-#             i += 1
-#
-# if path:
-#     print("y")
-# else:
-#     print("n")
 
 edge = {}
 for i in range(len(stations)):
@@ -85,7 +39,6 @@ while not visited[len(stations)-1] and len(q) != 0:
         print("n")
         break
     current = q.pop(0)
-    # print(q, current)
     if current == len(stations)-1:
         print("y")
         break
@@ -97,15 +50,9 @@ while not visited[len(stations)-1] and len(q) != 0:
             q.append(i)
             visited[i] = True
             previous[i] = current
-            # print(q, visited, previous)
 
     if len(q) == 0 and not all(value == True for value in visited.values()):
         print("n")
-    # else:
-    #     print("y")
-    #     break
 
 if len(stations)-1 in list(previous.keys()):
     print("y")
-# else:
-#     print("n")
